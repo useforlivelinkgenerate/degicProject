263975bcdbcc5b50f04157bd5732415c3a025917
useforlivelinkgenerate <jhzehan87@gmail.com>
2025-01-24 00:01:17 +0600

    resorces

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navs from "./Navbar";
+import HeroSec from "./HeroSection";
+import SecTwo from "./SectionTwo";
+import SliderFunc from "./SliderSec";
+import CardFunc from "./Card";
+function App() {
+
+
+  return (
+    <>
+      <Navs></Navs>
+      <HeroSec></HeroSec>
+      <SecTwo></SecTwo>
+      <SliderFunc></SliderFunc>
+      <CardFunc></CardFunc>
+    </>
+  )
+}
+
+export default App
